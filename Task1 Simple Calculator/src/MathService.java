f419ab97598d37855ccdd49682123d77ce29914a
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-05 22:34:24 +0500

    Add `MathService` interface and `MathServiceImpl` class with method stubs for basic operations.

---
+++ Task1 Simple Calculator/src/MathService.java
@@ -0,0 +1,7 @@
+
+public interface MathService {
+    int add(double firstNumber, double secondNumber);
+    int subtract(double firstNumber, double secondNumber);
+    int multiply(double firstNumber, double secondNumber);
+    int divide(double firstNumber, double secondNumber);
+}
---
+++ Task1 Simple Calculator/src/MathServiceImpl.java
@@ -0,0 +1,23 @@
+
+public class MathServiceImpl implements MathService {
+
+    @Override
+    public int add(double firstNumber, double secondNumber) {
+        return 0;
+    }
+
+    @Override
+    public int subtract(double firstNumber, double secondNumber) {
+        return 0;
+    }
+
+    @Override
+    public int multiply(double firstNumber, double secondNumber) {
+        return 0;
+    }
+
+    @Override
+    public int divide(double firstNumber, double secondNumber) {
+        return 0;
+    }
+}
