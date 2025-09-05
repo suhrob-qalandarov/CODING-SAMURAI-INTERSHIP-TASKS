511700f9e407bda241d12db051db4b782589459e
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-05 22:50:50 +0500

    Connect `SimpleCalculator` to `MathService` and implement operation handling with results display.

--- Task1 Simple Calculator/src/SimpleCalculator.java
+++ Task1 Simple Calculator/src/SimpleCalculator.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        MathService mathService = new MathServiceImpl();
         int chosenOperation;
-        double firstNumber, secondNumber;
+        double firstNumber, secondNumber, solution = 0;
         while (true){
             System.out.println("""
                     ------------------ INFO -------------------
@@ -26,6 +27,19 @@ public class SimpleCalculator {
             System.out.print(" Enter second number: ");
             secondNumber = scanner.nextDouble();
 
+            switch (chosenOperation) {
+                case 1 -> solution = mathService.add(firstNumber, secondNumber);
+                case 2 -> solution = mathService.subtract(firstNumber, secondNumber);
+                case 3 -> solution = mathService.multiply(firstNumber, secondNumber);
+                case 4 -> solution = mathService.divide(firstNumber, secondNumber);
+            }
+
+            System.out.printf("""
+                        
+                        -------------------------------------------
+                         Result is: %s
+                        
+                        %n""", solution);
         }
     }
 }
