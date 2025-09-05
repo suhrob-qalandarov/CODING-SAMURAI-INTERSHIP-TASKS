c5af976863ba106ad82e076de7520523a46517cc
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-06 00:34:43 +0500

    Final Refactor `SimpleCalculator` to enhance method modularity, improve readability, and streamline main execution flow.

--- src/Task1_SimpleCalculator/SimpleCalculator.java
+++ src/Task1_SimpleCalculator/SimpleCalculator.java
@@ -3,20 +3,36 @@ package Task1_SimpleCalculator;
 import java.util.Scanner;
 
 public class SimpleCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        MathService mathService = new MathServiceImpl();
-        int chosenOperation;
-        double firstNumber, secondNumber, solution = 0;
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final MathService mathService = new MathServiceImpl();
+
+    public static void main(String[] args) {
+        writeInfoMessage();
+
+        while (true){
+            int chosenOperation = mainMenu();
+
+            if (checkLogOut(chosenOperation)) break;
+
+            if (!checkOperation(chosenOperation)) continue;
+
+            writeChosenOptionMenu(chosenOperation);
+
+            toSolve(chosenOperation);
+        }
+    }
+
+    private static void writeInfoMessage() {
         System.out.println("""
                 
                     ------------------ INFO -------------------
                      This calculator would be able to add,
                      subtract, multiply and divide two numbers.""");
+    }
 
-        while (true){
-            chosenOperation = ScannerUtil.getIntDigit(scanner,"""
+    private static int mainMenu() {
+        return ScannerUtil.getIntDigit(SimpleCalculator.scanner,"""
                         -------------------------------------------
                          1 - Add
                          2 - Subtract
@@ -25,59 +41,72 @@ public class SimpleCalculator {
                         -------------------------------------------
                          0 - Exit
                          Choose:\s"""
-            );
+        );
+    }
 
-            if (chosenOperation == 0) {
-                System.out.println("""
+    private static boolean checkLogOut(int chosenOperation) {
+        if (chosenOperation == 0) {
+            System.out.println("""
+                            
                             -----------------
                              You logged out!
                             -----------------"""
-                );
-                return;
-            }
+            );
+            return true;
+        }
+        return false;
+    }
 
-            if (chosenOperation < 1 || chosenOperation > 4) {
-                System.out.println("""
-                            -!-
-                            ⚠️ Invalid option! Please choose between 0–4.
-                            -!-
-                            """
-                );
-                continue;
-            }
+    private static void writeChosenOptionMenu(int chosenOperation) {
+        String chosenOption = chosenOperation == 1 ? "Add"
+                : chosenOperation == 2 ? "Subtract"
+                : chosenOperation == 3 ? "Multiply"
+                : "Divide";
 
-            String chosenOption = chosenOperation == 1 ? "Add"
-                    : chosenOperation == 2 ? "Subtract"
-                    : chosenOperation == 3 ? "Multiply"
-                    : "Divide";
-
-            System.out.printf("""
+        System.out.printf("""
                         
                         --------------------------
                          Chosen option: %s
                         --------------------------""", chosenOption
-            );
-            firstNumber = ScannerUtil.getDoubleDigit(scanner, "\n Enter first number: ");
-            secondNumber = ScannerUtil.getDoubleDigit(scanner, " Enter second number: ");
+        );
+    }
 
-            try {
-                switch (chosenOperation) {
-                    case 1 -> solution = mathService.add(firstNumber, secondNumber);
-                    case 2 -> solution = mathService.subtract(firstNumber, secondNumber);
-                    case 3 -> solution = mathService.multiply(firstNumber, secondNumber);
-                    case 4 -> solution = mathService.divide(firstNumber, secondNumber);
-                }
+    private static void toSolve(int chosenOperation) {
+        double firstNumber, secondNumber, solution = 0;
 
-                System.out.printf("""
+        firstNumber = ScannerUtil.getDoubleDigit(scanner, "\n Enter first number: ");
+        secondNumber = ScannerUtil.getDoubleDigit(scanner, " Enter second number: ");
+
+        try {
+            switch (chosenOperation) {
+                case 1 -> solution = mathService.add(firstNumber, secondNumber);
+                case 2 -> solution = mathService.subtract(firstNumber, secondNumber);
+                case 3 -> solution = mathService.multiply(firstNumber, secondNumber);
+                case 4 -> solution = mathService.divide(firstNumber, secondNumber);
+            }
+
+            System.out.printf("""
                             --
                              Result is: %s
                             ---------------------------
                             
                             """, solution
-                );
-            } catch (ArithmeticException e) {
-                System.out.println(" ⚠️ Error: " + e.getMessage() + "\n");
-            }
+            );
+        } catch (ArithmeticException e) {
+            System.out.println(" ⚠️ Error: " + e.getMessage() + "\n");
         }
+    }
+
+    private static boolean checkOperation(int chosenOperation) {
+        if (chosenOperation < 1 || chosenOperation > 4) {
+            System.out.println("""
+                            -!-
+                            ⚠️ Invalid option! Please choose between 0–4.
+                            -!-
+                            """
+            );
+            return false;
+        }
+        return true;
     }
 }
